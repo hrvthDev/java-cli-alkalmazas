48c38acd11b325ea406c0852d9d0af31b56a1ccf
Nezáczki Áron <nezaczki.aron.2023@osztalyterem.katolikuskeri.hu>
2026-06-01 21:37:20 +0200

    számla fejléc dekorálása

--- src/service/InvoiceGenerator.java
+++ src/service/InvoiceGenerator.java
@@ -14,7 +14,7 @@ public class InvoiceGenerator {
 
         try(FileWriter writer = new FileWriter(fileName)) {
 
-            writer.write("===== SZÁMLA =====\n");
+            writer.write("******************* SZÁMLA *******************\n");
             writer.write("Rendelés ID: " + order.getOrderId() + "\n");
             writer.write("Dátum: " + order.getOrderDate() + "\n\n");
 
